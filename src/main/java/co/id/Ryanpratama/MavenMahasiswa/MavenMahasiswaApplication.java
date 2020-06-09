eacba2820a24f65f03f549b669a480d9c7b744cb
mrpratma12 <62782941+mrpratma12@users.noreply.github.com>
2020-06-09 13:12:35 -0700

    Add files via upload

---
+++ src/main/java/co/id/Ryanpratama/MavenMahasiswa/MavenMahasiswaApplication.java
@@ -0,0 +1,13 @@
+package co.id.RyanPratama.MavenMahasiswa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenMahasiswaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenMahasiswaApplication.class, args);
+	}
+
+}
